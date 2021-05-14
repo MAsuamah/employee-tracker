2a85c8828d5749a9a28f44db371d860483153ab2
Michelle <michey.asmah21@gmail.com>
2021-05-14 19:58:58 -0400

    tables created

---
+++ index.js
@@ -0,0 +1,14 @@
+const db = require('./db/connection');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+
+// Start server after DB connection
+db.connect(err => {
+  if (err) throw err;
+  console.log('Database connected.');
+  startApp();
+});
+
+function startApp() {
+  console.log('hey!')
+}
